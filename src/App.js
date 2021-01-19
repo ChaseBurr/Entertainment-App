9351028b9298c78f7c1a01ba8833c3b171fafa70
Chase <chase@chaseburr.com>
2021-01-19 16:20:38 -0700

    updated routes

--- src/App.js
+++ src/App.js
@@ -11,20 +11,21 @@ import MoviePage from "./Pages/MoviePage";
 import NotFound from "./Pages/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/movies" component={Movies} />
-          <Route exact path="/movies/:id" component={MoviePage} />
-          <Route exact path="/?search" component={MoviePage} />
-          {/* Needs to be at bottom */}
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Router>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Router>
+            <Switch>
+               {/* <Route exact path="/" component={Home} /> */}
+               <Route exact path="/Entertainment-App/" component={Home} />
+               <Route exact path="/movies" component={Movies} />
+               <Route exact path="/movies/:id" component={MoviePage} />
+               <Route exact path="/?search" component={MoviePage} />
+               {/* Needs to be at bottom */}
+               <Route path="*" component={NotFound} />
+            </Switch>
+         </Router>
+      </div>
+   );
 }
 
 export default App;
